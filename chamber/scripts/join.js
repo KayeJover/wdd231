2d0a3f3afccf0dc1584b5c80f86fa0ac0888e475
KayeJover <kayejover18@gmail.com>
2026-05-28 22:04:29 +0800

    message

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,7 +1,22 @@
 document.querySelector('#timestamp').value =
 new Date().toISOString();
 
-const lastModified = document.querySelector('#lastModified');
+const lastModified =
+document.querySelector('#lastModified');
 
 lastModified.textContent =
 `Last Modification: ${document.lastModified}`;
+
+const menuButton =
+document.querySelector('#menu');
+
+const navigation =
+document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+
+    navigation.classList.toggle('open');
+
+    menuButton.classList.toggle('open');
+
+});
---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,45 @@
+const params =
+new URLSearchParams(window.location.search);
+
+document.querySelector('#firstName').textContent =
+params.get('firstName');
+
+document.querySelector('#lastName').textContent =
+params.get('lastName');
+
+document.querySelector('#email').textContent =
+params.get('email');
+
+document.querySelector('#phone').textContent =
+params.get('phone');
+
+document.querySelector('#business').textContent =
+params.get('business');
+
+document.querySelector('#membership').textContent =
+params.get('membership');
+
+document.querySelector('#timestamp').textContent =
+params.get('timestamp');
+
+const lastModified =
+document.querySelector('#lastModified');
+
+lastModified.textContent =
+`Last Modification: ${document.lastModified}`;
+
+/* MOBILE MENU */
+
+const menuButton =
+document.querySelector('#menu');
+
+const navigation =
+document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+
+    navigation.classList.toggle('open');
+
+    menuButton.classList.toggle('open');
+
+});
